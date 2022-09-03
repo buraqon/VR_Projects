5a1936531bd1d1d3baf6a08ed3e18ec5e4b65a4d
Siraj Alaeli <ironsiraj222@gmail.com>
2022-09-03 12:43:50 +0300

    Initial Basic Scene

---
+++ VR Projects/vr projects_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/UnityEngine.VRModule_CodeGen.c
@@ -0,0 +1,85 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Boolean UnityEngine.XR.XRSettings::get_isDeviceActive()
+extern void XRSettings_get_isDeviceActive_m05C0035E5985667632BBE1BD972C611A674039DC (void);
+// 0x00000002 System.Single UnityEngine.XR.XRSettings::get_eyeTextureResolutionScale()
+extern void XRSettings_get_eyeTextureResolutionScale_m9B345E4B64424D6CEC0D15873B9777245EFADCE9 (void);
+// 0x00000003 System.Int32 UnityEngine.XR.XRSettings::get_eyeTextureWidth()
+extern void XRSettings_get_eyeTextureWidth_m3CE183B84E67605E54A6BEABB74B40B7088F2F45 (void);
+// 0x00000004 System.Int32 UnityEngine.XR.XRSettings::get_eyeTextureHeight()
+extern void XRSettings_get_eyeTextureHeight_m8EE6F89E72068B09B75DCCD94AC9438136507C28 (void);
+// 0x00000005 UnityEngine.Rendering.TextureDimension UnityEngine.XR.XRSettings::get_deviceEyeTextureDimension()
+extern void XRSettings_get_deviceEyeTextureDimension_mF47546BC51015DDA16B3E6B2A3089C41192850AF (void);
+// 0x00000006 System.String UnityEngine.XR.XRSettings::get_loadedDeviceName()
+extern void XRSettings_get_loadedDeviceName_m0EC0BC3BFBF1483DBC766D882A54677F2DBFC4BA (void);
+// 0x00000007 UnityEngine.XR.XRSettings/StereoRenderingMode UnityEngine.XR.XRSettings::get_stereoRenderingMode()
+extern void XRSettings_get_stereoRenderingMode_mB194A58846A080153AC66A75F422600EE1BF1C8C (void);
+// 0x00000008 System.Single UnityEngine.XR.XRDevice::get_refreshRate()
+extern void XRDevice_get_refreshRate_m5F492E6198C73E6DC60B5B81E3FE7B78ED84A555 (void);
+// 0x00000009 System.Boolean UnityEngine.XR.XRDevice::SetTrackingSpaceType(UnityEngine.XR.TrackingSpaceType)
+extern void XRDevice_SetTrackingSpaceType_mD5276BBB091C1AD8082D8F84F4DA0141E682C348 (void);
+// 0x0000000A System.Void UnityEngine.XR.XRDevice::DisableAutoXRCameraTracking(UnityEngine.Camera,System.Boolean)
+extern void XRDevice_DisableAutoXRCameraTracking_mDFDDD1D32A4D623A5448533B5E8E02B9536A25DC (void);
+// 0x0000000B System.Void UnityEngine.XR.XRDevice::InvokeDeviceLoaded(System.String)
+extern void XRDevice_InvokeDeviceLoaded_m07DEE6645B38728C7B8615DAAD6BE592C1DC59F9 (void);
+static Il2CppMethodPointer s_methodPointers[11] = 
+{
+	XRSettings_get_isDeviceActive_m05C0035E5985667632BBE1BD972C611A674039DC,
+	XRSettings_get_eyeTextureResolutionScale_m9B345E4B64424D6CEC0D15873B9777245EFADCE9,
+	XRSettings_get_eyeTextureWidth_m3CE183B84E67605E54A6BEABB74B40B7088F2F45,
+	XRSettings_get_eyeTextureHeight_m8EE6F89E72068B09B75DCCD94AC9438136507C28,
+	XRSettings_get_deviceEyeTextureDimension_mF47546BC51015DDA16B3E6B2A3089C41192850AF,
+	XRSettings_get_loadedDeviceName_m0EC0BC3BFBF1483DBC766D882A54677F2DBFC4BA,
+	XRSettings_get_stereoRenderingMode_mB194A58846A080153AC66A75F422600EE1BF1C8C,
+	XRDevice_get_refreshRate_m5F492E6198C73E6DC60B5B81E3FE7B78ED84A555,
+	XRDevice_SetTrackingSpaceType_mD5276BBB091C1AD8082D8F84F4DA0141E682C348,
+	XRDevice_DisableAutoXRCameraTracking_mDFDDD1D32A4D623A5448533B5E8E02B9536A25DC,
+	XRDevice_InvokeDeviceLoaded_m07DEE6645B38728C7B8615DAAD6BE592C1DC59F9,
+};
+static const int32_t s_InvokerIndices[11] = 
+{
+	6977,
+	7031,
+	6999,
+	6999,
+	6999,
+	7007,
+	6999,
+	7031,
+	6539,
+	6363,
+	6881,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_UnityEngine_VRModule_CodeGenModule;
+const Il2CppCodeGenModule g_UnityEngine_VRModule_CodeGenModule = 
+{
+	"UnityEngine.VRModule.dll",
+	11,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
